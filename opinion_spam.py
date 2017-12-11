32ef89575280dbe26290310c1f78d96bfcdbacd6
Max Schachere <maxschachere@gmail.com>
2017-12-10 23:47:29 -0500

    more features and accuracy evaluation

--- opinion_spam.py
+++ opinion_spam.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
+import nltk
 from sklearn import linear_model
+from sklearn.metrics import accuracy_score
 
 DATA_SET_PATH = "Data Sets/op_spam_v1.4/"
 
@@ -8,9 +10,7 @@ def main():
 	raw_training_data = load_data()
 	processed_data = featurize_data(raw_training_data)
 	model = create_model(processed_data)
-	
-	test = pd.DataFrame.from_dict([featurize_review("I had a wonderful time at young bloods farm on the hills!")])
-	print model.predict(test)
+	evaluate_model(model, processed_data)
 
 def load_data():
 	data = []
@@ -43,7 +43,26 @@ def featurize_review(review):
 	review_vector['length'] = len(review)
 	review_vector['word_count'] = len(words)
 	review_vector['average_word_length'] = sum(len(x) for x in words)/len(words)
+	tokens = nltk.word_tokenize(review)
+	pos_tags = nltk.pos_tag(tokens)
+	noun_count = 0
+	adjective_count = 0
+	first_person_count = 0
+	for tag in pos_tags:
+		word, tag = tag
+		if 'NN' in tag:
+			noun_count = noun_count + 1
+		if 'JJ' in tag:
+			adjective_count = adjective_count + 1
+		if word.lower() in ['i', 'we', 'me', 'us']:
+			first_person_count = first_person_count + 1
+	review_vector['num_nouns'] = noun_count
+	review_vector['num_adjectives'] = adjective_count
+	review_vector['first_person'] = first_person_count
 	return review_vector
+
+def get_named_entities():
+	pass	
 
 def create_model(data):
 	x = data.loc[:, data.columns != 'real']
@@ -52,5 +71,11 @@ def create_model(data):
 	logreg.fit(x, y)
 
 	return logreg
+
+def evaluate_model(model, data):
+	x = data.loc[:, data.columns != 'real']
+	y_true = data['real']
+	y_pred = model.predict(x)
+	print accuracy_score(y_true, y_pred)
 
 if __name__ == "__main__": main()
