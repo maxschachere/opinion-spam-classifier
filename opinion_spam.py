75fc67f24238a4a53ae195ce48d38a642e9c4a4e
Max Schachere <maxschachere@gmail.com>
2017-12-10 20:52:57 -0500

    first commit

---
+++ opinion_spam.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+from sklearn import linear_model
+
+DATA_SET_PATH = "Data Sets/op_spam_v1.4/"
+
+def main():
+	raw_training_data = load_data()
+	processed_data = featurize_data(raw_training_data)
+	model = create_model(processed_data)
+	
+	test = pd.DataFrame.from_dict([featurize_review("I had a wonderful time at young bloods farm on the hills!")])
+	print model.predict(test)
+
+def load_data():
+	data = []
+	real_reviews_path = DATA_SET_PATH + "real/"
+	fake_reviews_path = DATA_SET_PATH + "fake/"
+	for filename in os.listdir(real_reviews_path):
+		if filename.endswith(".txt"):
+			with open(real_reviews_path + filename) as f:
+				data.append({'review': f.readline(), 'real': True})
+	for filename in os.listdir(fake_reviews_path):
+		if filename.endswith(".txt"):
+			with open(fake_reviews_path + filename) as f:
+				data.append({'review': f.readline(), 'real': False})
+	return data
+
+def featurize_data(data):
+	processed_data = []
+	for review in data:
+		text = review['review']
+		review_vector = featurize_review(text)
+		review_vector['real'] = 'real' if review['real'] else 'fake'
+		processed_data.append(review_vector)
+
+	processed_data = pd.DataFrame.from_dict(processed_data)
+	return processed_data
+
+def featurize_review(review):
+	review_vector = {}
+	words = review.split(" ")
+	review_vector['length'] = len(review)
+	review_vector['word_count'] = len(words)
+	review_vector['average_word_length'] = sum(len(x) for x in words)/len(words)
+	return review_vector
+
+def create_model(data):
+	x = data.loc[:, data.columns != 'real']
+	y = data['real']
+	logreg = linear_model.LogisticRegression()
+	logreg.fit(x, y)
+
+	return logreg
+
+if __name__ == "__main__": main()
